8a88021aaea086c74840195b0664ec9ca7eb4b9e
Kennedy Njeri <mistakenz123@gmail.com>
2021-01-10 23:42:14 +0300

    added tests ii  to the project

--- tests/task.test.js
+++ tests/task.test.js
@@ -30,21 +30,21 @@ test('Should create task for user', async () => {
 })
 
 
-// test('fetch user tasks', async () => {
-//     const response = await request(app)
-//         .get(`/api/tasks/${userOneId}`)
-//         .set('Authorization', `Bearer ${userOne.token}`)
-//         .send().expect(200)
-//     expect(response.body.length).toEqual(2)
-// })
-//
-// // template string
-// test('should not delete other users task', async () => {
-//     await request(app)
-//         .delete(`/api/remove/tasks/${taskOne._id}/${userTwoId}`)
-//         .set('Authorization', `Bearer ${userTwo.token}`)
-//         .send().expect(404)
-//
-//     const task = await Task.findById(taskOne._id)
-//     expect(task).not.toBeNull()
-// })
+test('fetch user tasks', async () => {
+    const response = await request(app)
+        .get(`/api/tasks/${userOneId}`)
+        .set('Authorization', `Bearer ${userOne.token}`)
+        .send().expect(200)
+    expect(response.body.length).toEqual(2)
+})
+
+// template string
+test('should not delete other users task', async () => {
+    await request(app)
+        .delete(`/api/remove/tasks/${taskOne._id}/${userTwoId}`)
+        .set('Authorization', `Bearer ${userTwo.token}`)
+        .send().expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
